6a8a2934e0aac06736545d8b67262910d17316a4
raeben <45227695+daidowzi-raeben@users.noreply.github.com>
2022-05-12 17:53:35 +0900

    Create travel_wa.js

---
+++ travel_wa.js
@@ -0,0 +1,114 @@
+document.write(
+  '<link rel="stylesheet" type="text/css" href="https://www.hanacard.co.kr/css/headGnb_travel.css" />'
+);
+
+document.write('<div class="header_wrap">');
+document.write('<div class="header_cont">');
+document.write(
+  '<h1 class="logo"><a href="https://www.hanacard.co.kr/OPP99999998N.web?Code=travel"><img src="https://www.hanacard.co.kr/img/mega/layout/logo_travel.png" alt="하나카드 Travel" /></a></h1>'
+);
+document.write('<nav id="mega_gnb">');
+document.write("<ul>");
+document.write('<li class="gnb02 on">');
+document.write(
+  '<a href="https://www.hanacard.co.kr/OPP99999998N.web?Code=travel"></a>'
+);
+document.write('<ul class="dep2">');
+
+document.write('<li class="g4">');
+document.write('<a href="#">국내항공</a>');
+document.write('<ul class="dep3">');
+document.write(
+  '<li><a href="https://hncard.webtour.com/DA/da_index.asp" target="_top">웹투어</a></li>'
+);
+document.write("</ul>");
+document.write("</li>");
+
+document.write('<li class="g6">');
+document.write('<a href="#">국내여행</a>');
+document.write('<ul class="dep3">');
+document.write(
+  '<li><a href="https://hncard.webtour.com/_Tpl/hanacard/index.asp" target="_top">웹투어</a></li>'
+);
+document.write(
+  '<li><a href="http://hncd.verygoodtour.com/Affiliate/Product/DomesticMain" target="_top">참좋은여행</a></li>'
+);
+document.write("</ul>");
+document.write("</li>");
+
+document.write('<li class="g7">');
+document.write('<a href="#">국내숙박</a>');
+document.write('<ul class="dep3">');
+document.write(
+  '<li><a href="https://www.hanacard.co.kr/OPP99999998N.web?Code=stayhotelpass" target="_top">호텔패스</a></li>'
+);
+document.write(
+  '<li><a href="https://www.hanacard.co.kr/OPP99999998N.web?Code=staywebtour" target="_top">웹투어</a></li>'
+);
+document.write("</ul>");
+document.write("</li>");
+
+document.write('<li class="g8">');
+document.write('<a href="#">제주여행</a>');
+document.write('<ul class="dep3">');
+document.write(
+  '<li><a href="https://hncard.webtour.com/DGJ/dg_index.asp" target="_top">웹투어</a></li>'
+);
+document.write(
+  '<li><a href="http://hncd.verygoodtour.com/Affiliate/Product/DomesticMain" target="_top">참좋은여행</a></li>'
+);
+document.write("</ul>");
+document.write("</li>");
+
+document.write('<li class="g2">');
+document.write('<a href="#">해외패키지</a>');
+document.write('<ul class="dep3">');
+document.write(
+  '<li><a href="https://www.hanacard.co.kr/OPP99999998N.web?Code=hanatour" target="_top">하나투어</a></li>'
+);
+document.write(
+  '<li><a href="https://www.hanacard.co.kr/OPP99999998N.web?Code=modetour" target="_top">모두투어</a></li>'
+);
+document.write(
+  '<li><a href="http://hncd.verygoodtour.com/Affiliate/Product/PackageMain" target="_top">참좋은여행</a></li>'
+);
+document.write("</ul>");
+document.write("</li>");
+
+document.write('<li class="g3">');
+document.write('<a href="#">해외자유</a>');
+document.write('<ul class="dep3">');
+document.write(
+  '<li><a href="https://www.hanacard.co.kr/OPP99999998N.web?Code=naeiltour" target="_top">내일투어</a></li>'
+);
+document.write("</ul>");
+document.write("</li>");
+
+document.write('<li class="g5">');
+document.write('<a href="#">해외숙박</a>');
+document.write('<ul class="dep3">');
+document.write(
+  '<li><a href="https://www.hanacard.co.kr/OPP99999998N.web?Code=hotelpass" target="_top">호텔패스</a></li>'
+);
+document.write(
+  '<li><a href="https://www.hanacard.co.kr/OPP99999998N.web?Code=hotelhanatour" target="_top">하나투어</a></li>'
+);
+document.write(
+  '<li><a href="https://www.hanacard.co.kr/OPP99999998N.web?Code=hotelht" target="_top">내일투어</a></li>'
+);
+document.write("</ul>");
+document.write("</li>");
+
+document.write("</ul>");
+document.write("</li>");
+
+document.write("</ul>");
+document.write("</nav>");
+document.write(
+  '<a href="https://www.hanacard.co.kr/" target="_blank" title="하나카드 새창 열림" class="btn">하나카드 바로가기</a>'
+);
+document.write(
+  '<p class="txt">* 하나카드는 통신판매 중개자로서, 아래 상품은 해당 여행사의 책임하에 운영됩니다.</p>'
+);
+document.write("</div>");
+document.write("</div>");
